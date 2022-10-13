c4125e0343f697d1b7c6bee3c1e63f4df400ccc5
chanwoosung <chanwoosung@likelion.net>
2022-10-13 10:03:34 +0900

    Update: Change Nickname function

--- src/route/index.js
+++ src/route/index.js
@@ -9,9 +9,12 @@ router.get('/confirm', (req, res) => {
     Account.findOne({ip:req.query.ip},(err, account)=> {
     console.log(req.query,account)
     if(account) {
-        Account.updateOne({
-            ip: req.query.ip,
-        },{nickname: req.query.nickname});
+        (async()=>{
+            await Account.updateOne({
+                ip: req.query.ip,
+            },{nickname: req.query.nickname});
+            res.send({state:true});
+        })();
     } else {
         const newAccount = new Account({
             ip: req.query.ip,
@@ -23,7 +26,7 @@ router.get('/confirm', (req, res) => {
                 console.log(err);
             } else {
                 console.log(data);
-                res.send({state:true})
+                res.send({state:true});
             }
         })
     }
